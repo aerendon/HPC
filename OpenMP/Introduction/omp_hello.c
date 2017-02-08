2f69366a10ae57f531bebf62522cc29707b12def
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-02-08 18:43:44 -0500

    Init matrix multiplication

--- OpenMP/Introduction/omp_hello.c
+++ OpenMP/Introduction/omp_hello.c
@@ -18,8 +18,8 @@ int main (int argc, char *argv[]) {
   int nthreads, tid;
 
   /* Fork a team of threads giving them their own copies of variables */
-  #pragma omp parallel private(nthreads, tid) {
-
+  #pragma omp parallel private(nthreads, tid)
+  {
     /* Obtain thread number */
     tid = omp_get_thread_num();
     printf("Hello World from thread = %d\n", tid);
