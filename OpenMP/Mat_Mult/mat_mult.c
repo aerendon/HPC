0990130030bb513f50c675e5f6dc88e64e3b51be
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-02-10 22:11:45 -0500

    Time in test

--- OpenMP/Mat_Mult/mat_mult.c
+++ OpenMP/Mat_Mult/mat_mult.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <time.h>
 
 #define rowA 700
 #define colA 700
@@ -17,7 +18,7 @@ void initMatrix(int a[rowA][colA], int b[colA][colB], int sol[rowA][colB], int s
 
 	for (int i = 0; i < rowA; i++)
 		for (int j = 0; j < colB; j++)
-			sol[i][j] = sol_Open = 0;
+			sol[i][j] = sol_Open[i][j] = 0;
 }
 
 void showMatrix(int a[rowA][colA], int b[colA][colB]) {
@@ -42,15 +43,29 @@ void multMatrix(int a[rowA][colA], int b[colA][colB], int sol[rowA][colB]) {
 }
 
 void multMatrixOpen(int a[rowA][colA], int b[colA][colB], int sol[rowA][colB]) {
-	int i, j, k, rA = rowA, cA = colA, cB = colB;
-	#pragma omp parallel shared(a, b, rA, cA, cB), private(i, j, k, sol)
+	int i, j, k, rA = rowA, cA = colA, cB = colB, aux;
+	#pragma omp parallel shared(a, b, rA, cA, cB, sol), private(i, j, k, aux)
 	{
-		#pragma omp for scheduled(static)
-		for (int i = 0; i < rowA; i++)
-			for (int j = 0; j < colA; j++)
-				for (int k = 0; k < colA; k++)
-					sol[i][j] += a[i][k] *  b[k][j];
+		#pragma omp for schedule(static)
+		for (int i = 0; i < rowA; i++) {
+			for (int j = 0; j < colA; j++) {
+				aux = 0;
+				for (int k = 0; k < colA; k++) {
+					aux += a[i][k] *  b[k][j];
+				}
+				sol[i][j] = aux;
+			}
+		}
 	}
+}
+
+int check(int a[rowA][colB], int b[rowA][colB]) {
+	for (int i = 0; i < rowA; i++)
+		for (int j = 0; j < colB; j++) {
+			if (a[i][j] != b[i][j])
+				return 0;
+		}
+	return 1;
 }
 
 void showSol(int sol[rowA][colB]) {
@@ -65,11 +80,24 @@ void showSol(int sol[rowA][colB]) {
 
 int main() {
 	int a[rowA][colA], b[colA][colB], sol[rowA][colB], sol_Open[rowA][colB];
+	double start = 0, time_seq = 0, time_omp = 0;
 
 	initMatrix(a, b, sol, sol_Open);
+
+	start = omp_get_wtime();
 	multMatrix(a, b, sol);
+	time_seq = omp_get_wtime() - start;
+	start = omp_get_wtime();
 	multMatrixOpen(a, b, sol_Open);
+	time_omp = omp_get_wtime() - start;
+
+	if(check(sol, sol_Open)) {
+		printf("%s %f\n", "Time Sequential: ", time_seq);
+		printf("%s %f\n", "Time OMP: ", time_omp);
+	}
+	else printf("%s\n", "Wrong answer");
 	// showMatrix(a, b);
+	// showSol(sol);
 	// showSol(sol_Open);
 
 	return 0;
