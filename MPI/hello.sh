46811bcb1a756e487fc38576e55fbc11bb2fcdc6
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-02-23 23:25:16 -0500

    Slurm script to hellompi

---
+++ MPI/hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH --job-name=hello.c
+#SBATCH --output=hello.sout
+#
+#SBATCH --ntasks=1
+#SBATCH --nodes=3
+#SBATCH --time=10:00
+#SBATCH --mem-per-cpu=100
+
+mpirun /hello.out
