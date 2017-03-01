068a81d5d7aa8cb485d192fb9d97f80436b099d7
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-02-28 22:44:47 -0500

    Sending Matrix with MPI

--- MPI/Mat_Mult/mat_mult.c
+++ MPI/Mat_Mult/mat_mult.c
@@ -35,8 +35,8 @@ void showMatrix(int a[rowA][colA], int b[colA][colB]) {
 	}
 }
 
-void multMatrix(int a[rowA][colA], int b[colA][colB], int sol[rowA][colB]) {
-	for (int i = 0; i < rowA; i++)
+void multMatrix(int a[rowA][colA], int b[colA][colB], int sol[rowA][colB], int initRowA, int finRowA) {
+	for (int i = initRowA; i < finRowA; i++)
 		for (int j = 0; j < colA; j++)
 			for (int k = 0; k < colA; k++)
 				sol[i][j] += a[i][k] *  b[k][j];
@@ -64,25 +64,40 @@ void showSol(int sol[rowA][colB]) {
 int main() {
   MPI_Init(NULL, NULL);
 
-	int a[rowA][colA], b[colA][colB], sol[rowA][colB], sol_Open[rowA][colB];
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  char processor_name[MPI_MAX_PROCESSOR_NAME];
+  int name_len;
+  MPI_Get_processor_name(processor_name, &name_len);
+
+	int a[rowA][colA], b[colA][colB], sol[rowA][colB], sol_mpi[rowA][colB];
 	double start = 0, time_seq = 0, time_omp = 0;
   double t1, t2;
 
-	initMatrix(a, b, sol, sol_Open);
+	initMatrix(a, b, sol, sol_mpi);
 
-  t1 = MPI_Wtime();
-  multMatrix(a, b, sol);
-  t2 = MPI_Wtime();
-  printf( "Elapsed time is %f\n", t2 - t1 );
+  if (world_rank == 0) {
+    t1 = MPI_Wtime();
+    multMatrix(a, b, sol);
+    t2 = MPI_Wtime();
+    printf( "Secuential time is %f\n", t2 - t1 );
 
-	// if(check(sol, sol_Open)) {
-	// 	printf("%s %f\n", "Time Sequential: ", time_seq);
-	// 	printf("%s %f\n", "Time OMP: ", time_omp);
-	// }
-	// else printf("%s\n", "Wrong answer");
-	// showMatrix(a, b);
-	// showSol(sol);
-	// showSol(sol_Open);
+    t1 = MPI_Wtime();
+    vecAdd(a, b, sol_mpi, 0, col / 2);
+    MPI_Recv(sol_mpi + col/2, col - (col / 2), MPI_INT, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    t2 = MPI_Wtime();
+    printf( "MPI time is %f\n", t2 - t1 );
+    //showVector(sol_mpi);
+    // if(check(sol, sol_mpi)) printf("Correct\n");
+    // else printf(":( \n");
+
+  } else if (world_rank == 1) {
+    vecAdd(a, b, sol_mpi, col / 2, col);
+    MPI_Send(sol_mpi + col/2, col - (col / 2), MPI_INT, 0, 1, MPI_COMM_WORLD);
+    //showVector(sol_mpi);
+  }
 
   MPI_Finalize();
 	return 0;
