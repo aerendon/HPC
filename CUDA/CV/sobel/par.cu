803d26513d1ec0d32b88259253c3483169d50b9b
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-04-18 23:37:13 -0500

    GPU time

--- CUDA/CV/sobel/par.cu
+++ CUDA/CV/sobel/par.cu
@@ -122,7 +122,11 @@ int main(int argc, char** argv) {
     return -1;
   }
 
+  clock_t start = clock();
   create(image);
-
+  clock_t end = clock();
+  double gpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+  printf("%lf\n", gpu_time_used);
+  
   return 0;
 }
